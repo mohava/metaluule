df0acc9c0ab210e91106cf4d4b1cb85e7c95d7c6
KasPar <kaspar95@gmail.com>
2015-11-09 12:07:34 +0200

    Töötab

--- vanas6nade_import.py
+++ vanas6nade_import.py
@@ -1,18 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
 '''#defineeri siia funktisoon, mis võtab sisendiks sõna ja tagastab listi,
-#  kus on kõik päringust saadud vanasõnad (iga vanasõna on listi üks element)
-#def kysi_vanas6nad(sõna):'''
+#  kus on kõik päringust saadud vanasõnad (iga vanasõna on listi üks element) '''
+
+#def kysi_vanas6nad(sõna):
+sona = "naine"
+
+r = requests.post('http://www.folklore.ee/cgi-bin/script1', data = {"entry":sona})
+print(r.content) # selline html-kood on r.content -is
 
 
-#r = requests.get('http://www.folklore.ee/rl/date/robotid/leht1.html')
-#print(r.content)
-
-'''soup = BeautifulSoup(r.content)
-tag = soup.b
-print(tag)'''
-#r = requests.options('http://www.folklore.ee/rl/date/robotid/leht1.html')
-r = requests.post('http://www.folklore.ee/rl/date/robotid/leht1.html', data = {"/html/body/center/table[2]/tbody/tr[1]/td[1]/form/inFUCKFUCKFUCKput[1]":"mees"}) ###response 200
-print(r.text.encode("utf-8"))
-
+soup = BeautifulSoup(r.content, "html.parser")
+#print(soup.prettify())
+#print(soup.body.table.get_text())
+print(soup.body.table.get_text()) #ma proovin seda veel normaalsemale kujule saada
 
