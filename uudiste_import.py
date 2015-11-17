768bfe13f7faaf6084a18f302baaa95321a1d9d7
Mihkel Kohava <mihkelkohava@gmail.com>
2015-11-17 12:31:51 +0200

    Töötab

--- uudiste_import.py
+++ uudiste_import.py
@@ -4,4 +4,4 @@ from bs4 import BeautifulSoup
 
 def improdi_uudised(v6tms6na, uudisteportaalid (hulk)):
     #DELFI, ERR, PM, ÄRILEHT, NAISTEKS, ELU 24...
-    return pealkirjad # [{portaal, pealkiri).....]
+    return pealkirjad # [{portaal, pealkiri, kommentaaride arv).....]
