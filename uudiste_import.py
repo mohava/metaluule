cef76dbb825dcda1af5694e956011d9b81e401be
Mihkel Kohava <mihkelkohava@gmail.com>
2015-11-17 12:31:18 +0200

    Töötab

---
+++ uudiste_import.py
@@ -0,0 +1,7 @@
+__author__ = 'ATM'
+import requests
+from bs4 import BeautifulSoup
+
+def improdi_uudised(v6tms6na, uudisteportaalid (hulk)):
+    #DELFI, ERR, PM, ÄRILEHT, NAISTEKS, ELU 24...
+    return pealkirjad # [{portaal, pealkiri).....]
