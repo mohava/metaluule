e2b55791a987604c89bbcafbca81b5bc9832bae6
KasPar <kaspar95@gmail.com>
2015-11-19 12:50:18 +0200

    lemmatiseerija olemas

--- lemmatiseerija.py
+++ lemmatiseerija.py
@@ -5,10 +5,14 @@ def lemmatiseeri(s6na):
     sõnatüved = set()
     r = requests.get('http://www.filosoft.ee/lemma_et/lemma.cgi?word=' +s6na)
     soup = BeautifulSoup(r.content, "html.parser")
-    tüved = soup.body.br.get_text().split("\n")
+    try:
+        tüved = soup.body.br.get_text().split("\n")
+    except:
+        print("Tühi sõne sisendiks.")
+        return set()
     for element in tüved:
         if element != "" and element != "Copyright © 2013":
             sõnatüved.add(element)
     return sõnatüved #vahel on s6navormil mitu sobivat tüve kasin: kasima ja kasin
 
-#print(lemmatiseeri("kaczxca"))
+#print(lemmatiseeri(""))
