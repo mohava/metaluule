978d0822883e859254346cfd1b8fb575c1c5456a
KasPar <kaspar95@gmail.com>
2015-11-19 12:23:40 +0200

    lemmatiseerija olemas

--- lemmatiseerija.py
+++ lemmatiseerija.py
@@ -1,4 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
 #http://www.filosoft.ee/lemma_et/
 def lemmatiseeri(s6na):
-    s6natyved = []
-    return s6natyved #vahel on s6navormil mitu sobivat t�ve kasin: kasima ja kasin
+    sõnatüved = set()
+    r = requests.get('http://www.filosoft.ee/lemma_et/lemma.cgi?word=' +s6na)
+    soup = BeautifulSoup(r.content, "html.parser")
+    tüved = soup.body.br.get_text().split("\n")
+    for element in tüved:
+        if element != "" and element != "Copyright © 2013":
+            sõnatüved.add(element)
+    return sõnatüved #vahel on s6navormil mitu sobivat tüve kasin: kasima ja kasin
+
+#print(lemmatiseeri("kaczxca"))
