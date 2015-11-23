4e39aa2758c6aaad728a9fdec05e6c0c47e8641f
KasPar <kaspar95@gmail.com>
2015-11-23 20:56:40 +0200

    lemmatiseerija kodeerib sissetulevad sõnad utf-8sse

--- lemmatiseerija.py
+++ lemmatiseerija.py
@@ -1,18 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
 #http://www.filosoft.ee/lemma_et/
-def lemmatiseeri(s6na):
-    sõnatüved = set()
-    r = requests.get('http://www.filosoft.ee/lemma_et/lemma.cgi?word=' +s6na)
+def lemmatiseeri(sona):
+    sõnatyved = set()
+    r = requests.get('http://www.filosoft.ee/lemma_et/lemma.cgi?word=' +sona.encode("utf-8"))
     soup = BeautifulSoup(r.content, "html.parser")
     try:
-        tüved = soup.body.br.get_text().split("\n")
+        tyved = soup.body.br.get_text().split("\n")
     except:
         print("Tühi sõne sisendiks.")
         return set()
-    for element in tüved:
+    for element in tyved:
         if element != "" and element != "Copyright © 2013":
-            sõnatüved.add(element)
-    return sõnatüved #vahel on s6navormil mitu sobivat tüve kasin: kasima ja kasin
+            sõnatyved.add(element)
+    return sõnatyved #vahel on s6navormil mitu sobivat tüve kasin: kasima ja kasin
 
 #print(lemmatiseeri(""))
