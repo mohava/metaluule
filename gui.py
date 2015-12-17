3251492a2d60d8acff876bed1cfc8725e6736c0c
KasPar <kaspar95@gmail.com>
2015-12-17 09:21:33 +0200

    Luuletuse tegemiseks käivita programm gui.py

--- gui.py
+++ gui.py
@@ -6,7 +6,7 @@ def kuva_luuletus(*args):
     luuletus.set("LUULETUST GENEREERITAKSE...")
     root.update()
     s6na = str(võtmes6na.get())
-    kaalud=[round(asetus.get(),1),round(juhuslikkus.get(),1),round(rea_pikkus.get(),1),0.5,0.5,0.5,0.5]
+    kaalud=[index.get(),juhuslikkus.get(),rea_pikkus.get(),t2psus.get(),levik.get(),asetus.get(),t2psus.get()]
     #kui slaidereid ei liiguta, siis nende väärtuseks 0 !!!
     ridadelist = tee_luuletus(s6na, kaalud, eeltekst=[""], loendur=0, ridu=12)
     teksti_kujul = ""
@@ -17,7 +17,6 @@ def kuva_luuletus(*args):
     #    print("Viga gui.py-s")
 root = Tk()
 root.title("Luuletuse generaator")
-a
 mainframe = ttk.Frame(root, padding="3 3 12 12")
 mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
 mainframe.columnconfigure(0, weight=1)
@@ -42,23 +41,31 @@ ttk.Label(mainframe, image = image1).grid(column=1, row=2, sticky=E)
 ttk.Label(mainframe, image = image2).grid(column=3, row=2, sticky=E)
 
 #Slaiderid
-ttk.Label(mainframe, text="Juhuslikkus").grid(column=1, row=3, sticky=E)
+ttk.Label(mainframe, text="SAATUS").grid(column=1, row=3, sticky=E)
 juhuslikkus = ttk.Scale(mainframe, orient=HORIZONTAL, length=100, from_=0, to=0.5)
 juhuslikkus.grid(column=2, row=3)
 
-ttk.Label(mainframe, text="Rea pikkus").grid(column=1, row=4, sticky=E)
+ttk.Label(mainframe, text="TÕDE").grid(column=1, row=4, sticky=E)
 rea_pikkus = ttk.Scale(mainframe, orient=HORIZONTAL, length=100, from_=-1, to=0.1)
-rea_pikkus.grid(column=2, row=4, sticky=W)
+rea_pikkus.grid(column=2, row=4)
 
-ttk.Label(mainframe, text="Võtmesõna asetus").grid(column=1, row=5, sticky=E)
+ttk.Label(mainframe, text="RAUD JA KIVI").grid(column=1, row=5, sticky=E)
+index = ttk.Scale(mainframe, orient=HORIZONTAL, length=100, from_=-0.4, to=-0.7)
+index.grid(column=2, row=5)
+
+ttk.Label(mainframe, text="ARMASTUS").grid(column=1, row=6, sticky=E)
+t2psus = ttk.Scale(mainframe, orient=HORIZONTAL, length=100, from_=-0.4, to=-0.7)
+t2psus.grid(column=2, row=6)
+
+ttk.Label(mainframe, text="AU").grid(column=1, row=7, sticky=E)
+levik = ttk.Scale(mainframe, orient=HORIZONTAL, length=100, from_=-0.4, to=-0.7)
+levik.grid(column=2, row=7)
+
+ttk.Label(mainframe, text="ÕIGUS").grid(column=1, row=8, sticky=E)
 asetus = ttk.Scale(mainframe, orient=HORIZONTAL, length=100, from_=-0.4, to=-0.7)
-asetus.grid(column=2, row=5, sticky=W)
+asetus.grid(column=2, row=8)
 
-'''#spinbox
-rea_pikkus = StringVar()
-rea_pikkus = Spinbox(mainframe, from_=-1, to=0.1, textvariable=rea_pikkus,increment=0.1)
-rea_pikkus.grid(column=2, row=4, sticky=W)
-'''
+
 for child in mainframe.winfo_children(): child.grid_configure(padx=5, pady=5)
 
 võtmes6na_entry.focus()
