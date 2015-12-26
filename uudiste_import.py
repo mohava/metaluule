ec7596646bfe6b0557b6f90b0ac013f7193901d5
KasPar <kaspar95@gmail.com>
2015-12-26 13:16:54 +0200

    Uudiste import tagastab hulga, kus on AINULT pealkirjad.

--- uudiste_import.py
+++ uudiste_import.py
@@ -2,14 +2,25 @@ import requests
 from bs4 import BeautifulSoup
 #DELFI, ERR, PM, ÄRILEHT, NAISTEKAS??, ELU 24...
 def improdi_uudised():
-    r = requests.get("http://uudised.err.ee/uudised")
+    #r = requests.get("http://uudised.err.ee/uudised")
+    # soup = BeautifulSoup(r.content, "html.parser")
+    # #uudised = soup.html.body.div.div.div.encode("utf-8")
+    # #uudised = soup.ul.li.get_text().encode("utf-8")
+    r = requests.get('http://www.delfi.ee/archive/viimased/')
     soup = BeautifulSoup(r.content, "html.parser")
-    #vanas6nad = soup.html.body.div.div.div.encode("utf-8")
-    vanas6nad = soup.html.body.div.div.div.encode("utf-8")
-
-
-    print(vanas6nad)
+    #uudised = soup.body.ol.li.div.a.get_text() #saab ainult ühe uudise
+    uudised = soup.body.ol.get_text().strip()
+    list = uudised.split('\n')
+    hulk = set()
+    for element in list:
+        if element == '':
+            pass
+        elif element[0] == '\t' or element[0].isdigit():
+            pass
+        else:
+            hulk.add(element)
+    return hulk
     #return pealkirjad # [{portaal, pealkiri, kommentaaride arv).....]
 
 
-improdi_uudised()
+# print(improdi_uudised())
